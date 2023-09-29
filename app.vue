4ff299b6c670d128fe8c0753472420cc1d65970f
Lívia Simões <livias.simoes36@gmail.com>
2023-09-28 22:27:18 -0300

    Add favorites logic

--- app.vue
+++ app.vue
@@ -8,12 +8,11 @@
       <p><strong>Amount of time the response took to retrieve:</strong> {{ universities.apiPerformance.executionTime }}</p>
     </div>
 
-    <!-- TODO: O país "default" deve ser o Canadá -->
     <dropdown name="countries" :options="countries" @item-select="handleCountryChange" :selected="country"></dropdown>
 
     <search-box :searchTerm="search" @search-change="handleSearchChange"></search-box>
     <button @click="clearFilters">Clear Filters</button>
-    <list :rows="universities.items"></list>
+    <list :rows="universities.items" :favorites="favorites" :handleAdd="addToFavorites" :handleRemove="removeFromFavorites"></list>
   </div>
 </template>
 
@@ -24,6 +23,29 @@ const search = ref('');
 
 const handleRequest = () => {
   let startTime;
+  // TODO: Find better place for this
+  const _slugify = (term) => {
+    return term
+      .toString()
+      .toLowerCase()
+      .replace(/[àÀáÁâÂãäÄÅåª]+/g, "a") // Special Characters #1
+      .replace(/[èÈéÉêÊëË]+/g, "e") // Special Characters #2
+      .replace(/[ìÌíÍîÎïÏ]+/g, "i") // Special Characters #3
+      .replace(/[òÒóÓôÔõÕöÖº]+/g, "o") // Special Characters #4
+      .replace(/[ùÙúÚûÛüÜ]+/g, "u") // Special Characters #5
+      .replace(/[ýÝÿŸ]+/g, "y") // Special Characters #6
+      .replace(/[ñÑ]+/g, "n") // Special Characters #7
+      .replace(/[çÇ]+/g, "c") // Special Characters #8
+      .replace(/[ß]+/g, "ss") // Special Characters #9
+      .replace(/[Ææ]+/g, "ae") // Special Characters #10
+      .replace(/[Øøœ]+/g, "oe") // Special Characters #11
+      .replace(/[%]+/g, "pct") // Special Characters #12
+      .replace(/\s+/g, "-") // Replace spaces with -
+      .replace(/[^\w\-]+/g, "") // Remove all non-word chars
+      .replace(/\-\-+/g, "-") // Replace multiple - with single -
+      .replace(/^-+/, "") // Trim - from start of text
+      .replace(/-+$/, ""); // Trim - from end of text
+  };
 
   return $fetch(BASE_URL, {
     params: {country: country.value, name: search.value},
@@ -38,6 +60,7 @@ const handleRequest = () => {
         },
         items: response._data.map((university) => {
           return {
+            key: _slugify(`${university.name}-${university['state-province']}`),
             name: university.name,
             state: university['state-province'],
             website: university.web_pages[0] // TODO: Pegar todas as web pages
@@ -73,5 +96,71 @@ const handleCountryChange = async (countryName) => {
 const handleSearchChange = (searchTerm) => {
   search.value = searchTerm;
   refresh();
+}
+
+const favorites = ref([]);
+
+// Favorites DB
+let db; // objeto para manipular o BD
+
+if (process.client) {
+  let reqBD = indexedDB.open('universitiesdb', 1)
+
+  reqBD.onerror = function(evento) {
+    console.log(reqBD.error);
+  }
+  reqBD.onsuccess = function(evento) {
+    db = reqBD.result;
+      // Read all
+    db.transaction(['favorites'])
+      .objectStore('favorites')
+      .getAll().onsuccess = function(evento) {
+        console.log('RESULT', evento.target.result);
+        // favorites.value = evento.target.result;
+        favorites.value = evento.target.result.map(({key}) => key);
+    }
+  }
+
+  reqBD.onupgradeneeded = function(evento) {
+    db = reqBD.result;
+    if (!bd.objectStoreNames.contains('favorites'))
+      bd.createObjectStore('favorites', {keyPath: 'key'});
+  }
+
+
+}
+
+// Delete
+const removeFromFavorites = (key) => {
+  db.transaction(['favorites'], 'readwrite')
+    .objectStore('favorites')
+    .delete(key)
+    .onsuccess = function(evento) {
+      refreshFavorites();
+      // favorites.value = favorites.value.filter((favorite) => favorite.key !== key);
+      console.log('Livro excluído');
+    }
+  };
+
+// Add
+const addToFavorites = (university) => {
+  let reqOS = db.transaction(['favorites'], 'readwrite').objectStore('favorites').add({...university});
+
+  reqOS.onerror = function(evento) {
+    console.log(reqOS.error);
+  }
+  reqOS.onsuccess = function(evento) {
+    console.log(reqOS.result);
+    refreshFavorites();
+  }
+}
+
+const refreshFavorites = () => {
+  db.transaction(['favorites'])
+    .objectStore('favorites')
+    .getAll().onsuccess = function(evento) {
+      console.log('RESULT', evento.target.result);
+      favorites.value = evento.target.result.map(({key}) => key);
+    }
 }
 </script>
--- components/List.vue
+++ components/List.vue
@@ -9,11 +9,14 @@
       </tr>
     </thead>
     <tbody>
-      <tr v-for="row, i in rows" :key="i">
+      <tr v-for="row in rows" :key="row.key">
         <th>{{ row.name }}</th>
         <th>{{ row.state }}</th>
         <th>{{ row.website }}</th>
-        <th><button>View</button></th>
+        <th>
+          <button v-if="!favorites.includes(row.key)" @click="() => handleAdd(row)">Add to Favorites</button>
+          <button v-else @click="() => handleRemove(row.key)">Remove from Favorites</button>
+        </th>
       </tr>
     </tbody>
   </table>
@@ -24,7 +27,33 @@ const props = defineProps({
   rows: {
     type: Array,
     required: true
+  },
+  favorites: {
+    type: Array,
+    required: true
+  },
+  handleAdd: {
+    type: Function,
+    required: true
+  },
+  handleRemove: {
+    type: Function,
+    required: true
   }
 })
+// // const favorites = ref([]);
+// // const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
 
+// const handleAdd = () => {
+//   // const list = JSON.parse(localStorage.getItem('favorites')) || [];
+//   // favorites.push(row.key);
+//   // localStorage.setItem('favorites', JSON.stringify(list));
+// }
+
+// const remove = () => {
+//   // const list = JSON.parse(localStorage.getItem('favorites')) || [];
+//   // const index = list.indexOf(row.key);
+//   // list.splice(index, 1);
+//   // localStorage.setItem('favorites', JSON.stringify(list));
+// }
 </script>
